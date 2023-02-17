81b15b65c77bba7f6cd63aaf0cb10574b90423c8
Jeremy Carter <jeremy.a.carter97@gmail.com>
2023-02-17 12:10:20 -0500

    Add a function to check if the attacker hits or not

--- src/gameplay.py
+++ src/gameplay.py
@@ -25,3 +25,32 @@ def check_health(entity):
         return True
     else:
         return False
+
+
+def check_hit(attacker, defender, flag):
+    """Compares the attackers rolls to the defenders rolls"""
+    attack = roll_dice(attacker.dice)
+    defense = roll_dice(defender.dice)
+
+    if flag:
+        print("Attacker:", attack)
+        print("Defense:", defense)
+
+    if len(attack) > len(defense):
+        for x in range(len(defense)):
+            if attack[x] > defense[x]:
+                return True
+            elif defense[x] > attack[x]:
+                return False
+            else:
+                continue
+        return True
+    else:
+        for x in range(len(attack)):
+            if attack[x] > defense[x]:
+                return True
+            elif defense[x] > attack[x]:
+                return False
+            else:
+                continue
+        return False
