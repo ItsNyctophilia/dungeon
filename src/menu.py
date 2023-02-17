f6d581e8ed541d522122fd16f79c2c35f54f4714
Jeremy Carter <jeremy.a.carter97@gmail.com>
2023-02-17 11:00:29 -0500

    Move the Menu class into its own file

---
+++ src/menu.py
@@ -0,0 +1,27 @@
+class Menu:
+    def __init__(self):
+        self._selections = []
+
+    def __str__(self):
+        """Return a string of the printout of sorted menu options"""
+        menu_string = []
+        for idx, selection in enumerate(sorted(self._selections), start=1):
+            menu_string.append(f"{idx}. {selection}")
+        return "\n".join(menu_string)
+
+    @property
+    def selections(self):
+        """One of the selectable options in a menu"""
+        return self._selections
+
+    def has_selection(self, selection):
+        """Return True if selection is in menu, else False"""
+        return True if self._selections.count(selection) > 0 else False
+
+    def add_selection(self, new_selection):
+        """Add the given selection to the menu"""
+        self._selections.append(new_selection)
+
+    def del_selection(self, selection):
+        """Remove the given selection from the menu"""
+        self._selections.remove(selection)
