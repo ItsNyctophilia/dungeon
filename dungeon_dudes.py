4a6b8a1fbbfae85416464a77300ce57ad9222613
James Viner Esq <jrviner2000@gmail.com>
2023-02-16 21:48:52 -0500

    Implement basic Menu class functionality

--- dungeon_dudes.py
+++ dungeon_dudes.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 
 
+class Menu:
+    def __init__(self):
+        self._selections = []
+
+    @property
+    def selections(self):
+        """One of the selectable options in a menu"""
+        return self._selections
+
+    def has_selection(self, selection):
+        """Return True if selection is in menu, else False"""
+        return True if self._selections.count(selection) > 0 else False
+
+    def add_selection(self, new_selection):
+        """Add the given selection to the menu"""
+        self._selections.append(new_selection)
+
+    def del_selection(self, selection):
+        """Remove the given selection from the menu"""
+        self._selections.remove(selection)
+
+
 def main():
-    pass
+    m = Menu()
+    m.add_selection("Inventory")
+    m.add_selection("Explore")
+    m.add_selection("Status")
+    m.add_selection("Sense")
+    m.add_selection("Attack")
+    print(m)
 
 
 if __name__ == "__main__":
