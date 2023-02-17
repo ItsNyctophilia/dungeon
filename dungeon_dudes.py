af70a81ac5e789188eda7cec1c3c6d4971327f35
Jeremy Carter <jeremy.a.carter97@gmail.com>
2023-02-17 13:14:30 -0500

    Add being to fight a monster if they room generated one

--- dungeon_dudes.py
+++ dungeon_dudes.py
@@ -1,45 +1,28 @@
 #!/usr/bin/env python3
 
-import src.room
+
+from optparse import OptionParser
+import src.room as room
+import src.gameplay as game
 from src.menu import Menu
-from src.entity import Entity
 from src.monster import Monster
-
-class Menu:
-    def __init__(self):
-        self._selections = []
-
-    def __str__(self):
-        """Return a string of the printout of sorted menu options"""
-        menu_string = []
-        for idx, selection in enumerate(sorted(self._selections), start=1):
-            menu_string.append(f"{idx}. {selection}")
-        return "\n".join(menu_string)
-
-    @property
-    def selections(self):
-        """One of the selectable options in a menu"""
-        return self._selections
-
-    def has_selection(self, selection):
-        """Return True if selection is in menu, else False"""
-        return True if self._selections.count(selection) > 0 else False
-
-    def add_selection(self, new_selection):
-        """Add the given selection to the menu"""
-        self._selections.append(new_selection)
-
-    def del_selection(self, selection):
-        """Remove the given selection from the menu"""
-        self._selections.remove(selection)
+from src.hero import Hero
 
 
 def main():
+
+    parser = OptionParser()
+    parser.add_option("-d", "--dice", help="Print dice rolls to the screen.",
+                      action='store_true', default=False, dest='flag')
+    options = parser.parse_args
 
     main_menu = Menu()
     main_menu.add_selection("Inventory")
     main_menu.add_selection("Explore")
     main_menu.add_selection("Status")
+    main_menu.add_selection("Quit")
+
+    player = Hero()
 
     # ip == "introductory prompt"
     ip = "You awaken in a dark thicket, choked by overgrowth and drowned " \
@@ -50,29 +33,44 @@ def main():
          "rainwater dripping from the canopy above. Seeing no other " \
          "option, you bring yourself forward and grab ahold of it, " \
          "beginning your journey out of this blackened bog."
-    print(src.room.create_description_line(ip, src.room.get_flavor_line()),
+    print(room.create_description_line(ip, room.get_flavor_line()),
           main_menu, sep="")
 
     while (True):
         choice = input("> ")
         choice = choice.lower().strip()
-        # TODO: check if num_enemies > 0
-        # TODO: resolve_combat()
         if choice == "inventory":
 
             print("Placeholder loot bag")
 
         elif choice == "explore":
 
-            room = src.room.generate_room()
-            print(src.room.create_description_line(
-                  room.description, src.room.get_flavor_line()),
+            room_ = room.generate_room()
+            print(room.create_description_line(
+                  room_.description, room.get_flavor_line()),
                   main_menu, sep="")
+            if room_.num_foes > 0:
+                if not main_menu.has_selection("Fight"):
+                    main_menu.add_selection("Fight")
+            else:
+                if main_menu.has_selection("Fight"):
+                    main_menu.del_selection("Fight")
             continue
 
         elif choice == "status":
 
-            print("Placeholder health value")
+            print("Current Health:", player.hp)
+
+        elif choice == "quit":
+
+            print("Exiting....")
+            exit()
+
+        elif choice == "fight":
+            monster = Monster.generate_monster()
+            initiative = game.roll_initiative()
+            game.combat(player, monster, initiative, options)
+            print(main_menu)
 
         else:
 
