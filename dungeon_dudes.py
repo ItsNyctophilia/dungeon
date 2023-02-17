c13beb8b9f1e90d83508d6b5c21bc89ab3673f19
James Viner Esq <jrviner2000@gmail.com>
2023-02-16 22:16:28 -0500

    Add Room class for future room creation

--- dungeon_dudes.py
+++ dungeon_dudes.py
@@ -1,5 +1,48 @@
 #!/usr/bin/env python3
 
+import random
+
+
+class Room:
+    def __init__(self, description, num_foes=-1):
+        self._description = description
+        self._num_foes = num_foes
+        self._treasure = []
+
+    def __str__(self):
+        return f"{self._description}\n{self._num_foes}\n{self._treasure}"
+
+    @property
+    def description(self):
+        """World-building description of the room"""
+        return self._description
+
+    @property
+    def num_foes(self):
+        """Number of enemies remaining in the room"""
+        return self._num_foes
+
+    @num_foes.setter
+    def num_foes(self, num_foes):
+        self._num_foes = num_foes
+
+    @property
+    def treasure(self):
+        """List of the treasure items found in the room"""
+        return self._treasure
+
+    @treasure.setter
+    def treasure(self, treasure):
+        self._treasure = treasure
+
+    def add_treasure(self, new_treasure):
+        """Add the given treasure to the room"""
+        self._treasure.append(new_treasure)
+
+    def del_treasure(self, treasure):
+        """Remove the given treasure from the room"""
+        self._treasure.remove(treasure)
+
 
 class Menu:
     def __init__(self):
@@ -38,6 +81,9 @@ def main():
     m.add_selection("Sense")
     m.add_selection("Attack")
     print(m)
+
+    r = Room("This is a placeholder.", 20)
+    print(r)
 
 
 if __name__ == "__main__":
