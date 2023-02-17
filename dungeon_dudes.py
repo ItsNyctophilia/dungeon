bee97150cf0a6922f1f6cbeff6481b1ce7124970
James Viner Esq <jrviner2000@gmail.com>
2023-02-16 21:45:42 -0500

    Add dungeon_dudes.py file

---
+++ dungeon_dudes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (Exception, GeneratorExit, KeyboardInterrupt, SystemExit) as e:
+        name = type(e).__name__
+        print("Exception of type", name, "prevented program from continuing!")
