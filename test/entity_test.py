486097970b9becfb4ed42a0aff93f45eb18ff4cb
Jeremy Carter <jeremy.a.carter97@gmail.com>
2023-02-18 09:40:38 -0500

    Add unit tests for the monster class

--- test/entity_test.py
+++ test/entity_test.py
@@ -13,6 +13,10 @@ class TestEntity(unittest.TestCase):
         self.assertEqual(self.entity_.hp, 5)
         self.assertEqual(self.entity_.dice, 3)
         self.assertFalse(self.entity_.treasure)
+    
+    def test_entity_hit(self):
+        self.entity_.hit()
+        self.assertEqual(self.entity_.hp, 4)
 
 
 if __name__ == "__main__":
